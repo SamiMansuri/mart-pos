37668be88b141b7c87265ef5a136bb2767cf29b1
Sami Mansuri <92858689+SamiMansuri@users.noreply.github.com>
2026-03-25 11:08:46 +0530

    Update ProductsList.jsx

--- client/src/pages/ProductsList.jsx
+++ client/src/pages/ProductsList.jsx
@@ -371,7 +371,7 @@ const ProductsList = () => {
                       >
                         <EditIcon fontSize="small" />
                       </IconButton>
-                      {isAdminUser && (
+                      {(
                         <IconButton
                           size="small"
                           color="error"
